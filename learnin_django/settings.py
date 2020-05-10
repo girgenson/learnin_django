9238de732f595df9fc83c4f57da36b41794b2e1a
girgenson <vadimgirgenson@yandex.ru>
2020-05-10 23:40:58 +0300

    Initial commit

--- learnin_django/settings.py
+++ learnin_django/settings.py
@@ -10,7 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+
+
+from dotenv import load_dotenv
 from pathlib import Path
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qc$%&dbe)_2mz4(4abj#4u9+)ff_0)%jgdagl!h-&p(h_lx0lo'
+SECRET_KEY = os.getenv("THEKEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
