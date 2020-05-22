ce00f13fe1b2105b6762cc9cd68789bbefcd5361
girgenson <vadimgirgenson@yandex.ru>
2020-05-22 07:53:06 +0300

    Reorganizing 'index.html'

--- news/views.py
+++ news/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from . models import News
+from . models import News, Category
 
 
 def index(request):
     news = News.objects.order_by('-created_at')
-    context = {'news': news, 'title': 'Список новостей'}
+    categories = Category.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей',
+        'category': categories
+        }
     return render(request, 'news/index.html', context)
