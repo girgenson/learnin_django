b93e75700d653ae0e20a3257238e8693e8bb239c
girgenson <vadimgirgenson@yandex.ru>
2020-05-27 14:00:18 +0300

    Reorganize of html structure, 'get_category' view

--- news/views.py
+++ news/views.py
@@ -8,6 +8,20 @@ def index(request):
     context = {
         'news': news,
         'title': 'Список новостей',
-        'category': categories
+        'categories': categories
         }
+    for i in categories:
+        print(i)
     return render(request, 'news/index.html', context)
+
+
+def get_category(request, category_id):
+    categories = Category.objects.all()
+    news = News.objects.filter(category_id=category_id)
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'news': news,
+        'categories': categories,
+        'category': category
+    }
+    return render(request, 'news/category.html', context)
