c12f7533f906b323a09c18c780a5ebc509af6ce7
girgenson <vadimgirgenson@yandex.ru>
2020-07-04 19:23:04 +0300

    Created class-based view instead of 'add_news' function

--- news/urls.py
+++ news/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('category/<int:category_id>', NewsByCategory.as_view(), name='category'),
     # path('<int:news_id>', view_news, name='view_news'),
     path('<int:pk>', ViewNews.as_view(), name='view_news'),
-    path('add_news', add_news, name='add_news'),
+    # path('add_news', add_news, name='add_news'),
+    path('add_news', CreateNews.as_view(), name='add_news'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from . models import News, Category
 from . forms import NewsForm
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 
 class HomeNews(ListView):
@@ -41,6 +42,13 @@ class ViewNews(DetailView):
     #template_name = 'news/news_detail.html'
 
 
+class CreateNews(CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    # success_url = reverse_lazy('home')
+
+
+
 # def index(request):
 #     news = News.objects.order_by('-created_at')
 #     context = {
@@ -50,31 +58,31 @@ class ViewNews(DetailView):
 #     return render(request, 'news/index.html', context)
 
 
-def get_category(request, category_id):
-    categories = Category.objects.all()
-    news = News.objects.filter(category_id=category_id)
-    context = {
-        'news': news,
-        'categories': categories,
-    }
-    return render(request, 'news/category.html', context)
+# def get_category(request, category_id):
+#     categories = Category.objects.all()
+#     news = News.objects.filter(category_id=category_id)
+#     context = {
+#         'news': news,
+#         'categories': categories,
+#     }
+#     return render(request, 'news/category.html', context)
+#
+#
+# def view_news(request, news_id):
+#     # news_item = News.objects.get(pk=news_id)
+#     news_item = get_object_or_404(News, pk=news_id)
+#     return render(request, 'news/view_news.html', {"news_item": news_item})
 
 
-def view_news(request, news_id):
-    # news_item = News.objects.get(pk=news_id)
-    news_item = get_object_or_404(News, pk=news_id)
-    return render(request, 'news/view_news.html', {"news_item": news_item})
-
-
-def add_news(request):
-    if request.method == 'POST':
-        form = NewsForm(request.POST)  # Форма, связанная с данными
-        # Пользователю не нужно будет ее повторно заполнять при неверном заполнении
-        if form.is_valid():
-            # print(form.cleaned_data)
-            # news = News.objects.create(**form.cleaned_data)
-            news = form.save()
-            return redirect(news)
-    else:
-        form = NewsForm()  # Форма, не связанная с данными
-    return render(request, 'news/add_news.html', {'form': form})
+# def add_news(request):
+#     if request.method == 'POST':
+#         form = NewsForm(request.POST)  # Форма, связанная с данными
+#         # Пользователю не нужно будет ее повторно заполнять при неверном заполнении
+#         if form.is_valid():
+#             # print(form.cleaned_data)
+#             # news = News.objects.create(**form.cleaned_data)
+#             news = form.save()
+#             return redirect(news)
+#     else:
+#         form = NewsForm()  # Форма, не связанная с данными
+#     return render(request, 'news/add_news.html', {'form': form})
