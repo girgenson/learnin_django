09160d4c1370d1fe1687541c330eb4192b8ef7fa
girgenson <vadimgirgenson@yandex.ru>
2020-07-13 19:57:06 +0300

    Pagination

--- news/views.py
+++ news/views.py
@@ -2,35 +2,47 @@ from django.shortcuts import render, get_object_or_404, redirect
 from . models import News, Category
 from . forms import NewsForm
 from django.views.generic import ListView, DetailView, CreateView
-from django.urls import reverse_lazy
+from . utils import MyMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 
 
-class HomeNews(ListView):
+def test(request):
+    objects = ['john1', 'paul2', 'george3', 'ringo4', 'flloyd5', 'dubb6', 'tashiko7', 'john8', 'rock9']
+    paginator = Paginator(objects, 2)
+    page_num = request.GET.get('page', 1)
+    page_objects = paginator.get_page(page_num)
+    return render(request, 'news/test.html', {'page_obj': page_objects})
+
+
+class HomeNews(MyMixin, ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
-    # extra_context = {'title': 'Главная'}
-    queryset = News.objects.select_related('category')
+    mixin_prop = 'hello world'
+    paginate_by = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная страница'
+        context['title'] = self.get_upper('Главная страница')
+        context['mixin_prop'] = self.get_prop()
         return context
 
     def get_queryset(self):
         return News.objects.filter(is_published=True).select_related('category')
 
 
-class NewsByCategory(ListView):
+class NewsByCategory(MyMixin, ListView):
     model = News
     # Используем тот же самый шаблон потому что вывод ничем не отличается:
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
     allow_empty = False
+    paginate_by = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        context['title'] = self.get_upper(Category.objects.get(pk=self.kwargs['category_id']))
         return context
 
     def get_queryset(self):
@@ -44,10 +56,13 @@ class ViewNews(DetailView):
     #template_name = 'news/news_detail.html'
 
 
-class CreateNews(CreateView):
+class CreateNews(LoginRequiredMixin, CreateView):
     form_class = NewsForm
     template_name = 'news/add_news.html'
     # success_url = reverse_lazy('home')
+    # login_url = '/admin'
+    # login_url = reverse_lazy('home')
+    raise_exception = True
 
 
 # def index(request):
