14dfb30512cc61c3231138f69a3f880c6be4b54c
girgenson <vadimgirgenson@yandex.ru>
2020-07-24 13:40:43 +0300

    Autorization, autentication

--- news/views.py
+++ news/views.py
@@ -1,30 +1,44 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from . models import News, Category
-from . forms import NewsForm, UserRegisterForm
+from . forms import NewsForm, UserRegisterForm, UserLoginForm
 from django.views.generic import ListView, DetailView, CreateView
 from . utils import MyMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth import login, logout
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserLoginForm()
+    return render(request, 'news/login.html', {'form': form})
 
 
 def register(request):
     if request.method == 'POST': 
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             messages.success(request, 'Вы успешно зарегистрировались')
-            return redirect('login')
+            return redirect('home')
         else:
             messages.error(request, 'Ошибка регистрации')
     else:
         form = UserRegisterForm()
     return render(request, 'news/register.html', {'form': form})
-
-
-def user_login(request):
-    return render(request, 'news/login.html')
 
 
 def test(request):
